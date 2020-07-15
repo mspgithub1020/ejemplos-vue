3f6300eccea499dc13f9d86e386fc1dfe599c8da
mspgithub <mspformacion1020@gmail.com>
2020-07-15 13:33:58 +0200

    ejercicio2

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,31 @@
     <!--<img alt="Vue logo" src="./assets/logo.png">-->
     <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <Hola/>
+    <Formulario/>
   </div>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
 import Hola from './components/ejercicio1.vue'
+import Formulario from './components/ejercicio2.vue'
+
+import Vue from 'vue';
+import { MdButton, MdField, MdInput, Label } from 'vue-material/dist/components';
+Vue.use(MdButton);
+Vue.use(MdField);
+Vue.use(MdInput);
+Vue.use(Label);
+
+
+
 
 export default {
   name: 'App',
   components: {
     //HelloWorld
-    Hola
+    Hola,
+    Formulario
   }
 }
 </script>
--- src/components/ejercicio2.vue
+++ src/components/ejercicio2.vue
@@ -5,3 +5,48 @@ El email debe tener un formato adecuado
 contraseña debe tener al menos 6 caracteres. Mostrar los errores correspondientes 
 después de escribir en cada uno de los campos y cuando se envíe el formulario.
 -->
+
+<template>
+  <form>
+    <md-field>
+      <label>Email</label>
+      <md-input v-model="email"></md-input>
+    </md-field>
+
+    <md-field>
+      <label>Password</label>
+      <md-input v-model="password"></md-input>
+    </md-field>
+
+    <md-button class="md-primary">Enviar</md-button>
+
+  </form>
+</template>
+
+<script>
+/*
+import Vue from 'vue';
+import { MdButton, MdField, MdInput, Label } from 'vue-material/dist/components';
+Vue.use(MdButton);
+Vue.use(MdField);
+Vue.use(MdInput);
+Vue.use(Label);*/
+
+export default {
+    name : "Formulario",
+
+    data(){
+        return {
+            email : '',
+            password : ''
+
+        }
+    },
+
+
+
+};
+</script>
+
+<style scoped>
+</style>

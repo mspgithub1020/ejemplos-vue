2ec945f85d29c061f2462bfdc57f377661f90536
mspgithub <mspformacion1020@gmail.com>
2020-07-16 09:04:35 +0200

    ejercicio2

--- src/App.vue
+++ src/App.vue
@@ -12,16 +12,6 @@
 import Hola from './components/ejercicio1.vue'
 import Formulario from './components/ejercicio2.vue'
 
-import Vue from 'vue';
-import { MdButton, MdField, MdInput, Label } from 'vue-material/dist/components';
-Vue.use(MdButton);
-Vue.use(MdField);
-Vue.use(MdInput);
-Vue.use(Label);
-
-
-
-
 export default {
   name: 'App',
   components: {
@@ -34,11 +24,16 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  
+  margin-top: 60px;
+}
+</style>
+
+
+<!--
+font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+-->
--- src/components/ejercicio2.vue
+++ src/components/ejercicio2.vue
@@ -7,46 +7,106 @@ después de escribir en cada uno de los campos y cuando se envíe el formulario.
 -->
 
 <template>
-  <form>
-    <md-field>
-      <label>Email</label>
-      <md-input v-model="email"></md-input>
-    </md-field>
+  <form novalidate class="md-layout" @submit.prevent.stop="onSubmit">
+      <md-card class="md-layout-item md-size-50 md-small-size-100">
+        <md-card-header>
+          <div class="md-title">Login</div>
+        </md-card-header>
 
-    <md-field>
-      <label>Password</label>
-      <md-input v-model="password"></md-input>
-    </md-field>
+        <md-card-content>
 
-    <md-button class="md-primary">Enviar</md-button>
+          <md-field >
+            <label for="emailID">Email</label>
+            <md-input id="emailID" type="email" name="email" @blur="validateEmail"  autocomplete="email" v-model="form.email" :disabled="sending" />
+            <p role="alert" v-if="emailError">{{emailError}}</p>
+          </md-field>
 
-  </form>
+          <md-field >
+            <label for="passwordID">Password</label>
+            <md-input id="passwordID" name="password" @blur="validatePassword" autocomplete="family-name" v-model="form.lastName" :disabled="sending" />
+            <p role="alert" v-if="passwordError">{{passwordError}}</p>
+          </md-field>
+
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button type="submit" class="md-primary" >Enviar</md-button>
+        </md-card-actions>
+      </md-card>
+
+      
+    </form>
+
 </template>
 
 <script>
-/*
-import Vue from 'vue';
-import { MdButton, MdField, MdInput, Label } from 'vue-material/dist/components';
-Vue.use(MdButton);
-Vue.use(MdField);
-Vue.use(MdInput);
-Vue.use(Label);*/
+
+/* eslint-disable no-debugger */
+
+import uuid from 'uuid';
+
+const isEmail = (value) => {
+
+  const isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
+  return isValidEmail ? '' : 'Formato de email incorrecto';
+
+}
+
+const isPassword = (value) => {
+
+  //Para verificar una contraseña de entre 6 y 20 caracteres que contenga al menos un 
+  //dígito numérico, una letra mayúscula y una letra minúscula
+  const isValidPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/.test(value);
+  return isValidPassword ? '' : 'El password debe contener entre 6 a 20 caracteres  , al menos un digito , una letra mayuscula y una minuscula';
+
+}
 
 export default {
     name : "Formulario",
-
     data(){
-        return {
+      return {
+            emailID : uuid.v4(),
             email : '',
-            password : ''
+            password : '',
+            passwordID : uuid.v4(),
+            emailerror : '',
+            passwordError : ''
 
         }
     },
+    methods: {
+      onSubmit(){
+        this.validateForm();
+        if(this.isFormValid()){
+          console.log({email:this.email, password: this.password})
+        }
+      },
+      validateForm(){
+        this.validateEmail;
+      },
+      isFormValid(){
+        return !this.emailError && !this.passwordError;
+      },
+      validateEmail(){        
+        this.emailError = isEmail(this.email);
+      },
+      validatePassword(){        
+        this.passwordError = isPassword(this.password);
+      }
 
-
+    }
 
 };
 </script>
 
 <style scoped>
 </style>
+
+
+<!--<span class="md-error" v-if="!$v.form.email.required">The email is required</span>
+            <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
+            
+    <span class="md-error" v-if="!$v.form.lastName.required">The last name is required</span>
+            <span class="md-error" v-else-if="!$v.form.lastName.minlength">Invalid last name</span>
+            
+            -->
